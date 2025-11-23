e5a240ff8a8d6ff29c7293cb943752730d61748f
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-23 22:26:00 +0200

    feat(cart): implement quantity-based cart with add-to-cart logic

--- src/components/ShoppingCartIcon.tsx
+++ src/components/ShoppingCartIcon.tsx
@@ -3,12 +3,11 @@ import { useCart } from "../context/CartContext";
 
 const ShoppingCartIcon = () => {
   const { cart } = useCart();
-  const count = cart?.length ?? 0;
+  const count = cart?.reduce((sum, cartItem) => sum + cartItem.quantity, 0);
 
   return (
     <div className="fixed top-10 right-10 inline-flex items-center justify-center p-3 bg-indigo-50 text-indigo-600 rounded-2xl shadow-md hover:shadow-lg transition-shadow cursor-pointer">
       <HiShoppingCart className="text-3xl" />
-
       <span className="absolute -top-1 -right-1 flex items-center justify-center w-5 h-5 text-xs font-semibold text-white bg-indigo-600 rounded-full shadow">
         {count}
       </span>
--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useState, type ReactNode } from "react";
 import type { Product } from "../types";
+type CartItem = { product: Product; quantity: number };
 type CartState = {
-  cart?: Product[];
-  setCart?: React.Dispatch<React.SetStateAction<Product[]>>;
+  cart?: CartItem[];
+  addToCart?: (v: Product) => void;
 };
 
 const CartContext = createContext<CartState>({});
@@ -11,9 +12,23 @@ const CartContext = createContext<CartState>({});
 export const CartProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  const [cart, setCart] = useState<Product[]>([]);
+  const [cart, setCart] = useState<CartItem[]>([]);
+  const addToCart = (product: Product) => {
+    setCart!((prev) => {
+      const existing = prev.find((c) => c.product.id === product.id);
+      if (existing) {
+        return prev.map((item) =>
+          item.product.id === product.id
+            ? { product: item.product, quantity: ++item.quantity }
+            : item
+        );
+      } else {
+        return [...prev, { product, quantity: 1 }];
+      }
+    });
+  };
   return (
-    <CartContext.Provider value={{ cart, setCart }}>
+    <CartContext.Provider value={{ cart, addToCart }}>
       {children}
     </CartContext.Provider>
   );
